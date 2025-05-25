047d336b848e09dd2e9a0f20e67dcbf1ecc5bc12
Jeremy Sheeshka <Jsheeshka@gmail.com>
2025-05-25 14:10:24 -0700

    edit

--- src/_data/whatido.js
+++ src/_data/whatido.js
@@ -9,7 +9,7 @@ export default {
     },
     {
       key: 'lessons',
-      title: 'Lessons',
+      title: 'Music Lessons',
       summary:
         "Let's work together to find a a method of learning the guitar for you. I can help you upgrade your improvisation, music theory, and compositional skills through a variety of resources and exercises to keep you learning.",
       link: ''
@@ -18,7 +18,7 @@ export default {
       key: 'webdesign',
       title: 'Web Design',
       summary:
-        'A creative website can be a unique way to showcase the essence of a project, business or product. I offer responsive web design services that specifically cater to small local businesses, entrepreneurs and artists in Northern B.C. Canada',
+        'A creative website can be a unique way to showcase the essence of a project, business or product. I offer responsive web design services that specifically cater to small local businesses, entrepreneurs and artists in Northern B.C., Canada.',
       link: ''
     },
   ],
