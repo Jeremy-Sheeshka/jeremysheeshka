bba8fa22ddede706dedc4edacc6fef5d57eac46e
Jeremy Sheeshka <jsheeshka@gmail.com>
2026-03-01 18:14:14 -0800

    hoa 3 done

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,9 +2,10 @@
 export default new Map([
 ["src/content/post/2025-01-26-IP-8-ETEC544.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2025-01-26-IP-8-ETEC544.mdx&astroContentModuleFlag=true")],
 ["src/content/post/2025-06-18-good-example-of-qualitative-research.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2025-06-18-good-example-of-qualitative-research.mdx&astroContentModuleFlag=true")],
-["src/content/post/2026-01-05-etec544-ip1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-01-05-etec544-ip1.mdx&astroContentModuleFlag=true")],
 ["src/content/post/2026-01-05-blogpost-template-ideas.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-01-05-blogpost-template-ideas.mdx&astroContentModuleFlag=true")],
+["src/content/post/2026-01-05-etec544-ip1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-01-05-etec544-ip1.mdx&astroContentModuleFlag=true")],
+["src/content/post/2026-01-17-etec542-hoa1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-01-17-etec542-hoa1.mdx&astroContentModuleFlag=true")],
 ["src/content/post/2026-02-05-etec542-HOA2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-02-05-etec542-HOA2.mdx&astroContentModuleFlag=true")],
 ["src/content/post/2026-02-07-etec544-ch6fullerton.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-02-07-etec544-ch6fullerton.mdx&astroContentModuleFlag=true")],
-["src/content/post/2026-01-17-etec542-hoa1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-01-17-etec542-hoa1.mdx&astroContentModuleFlag=true")]]);
+["src/content/post/2026-03-1-etec542-HOA3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-03-1-etec542-HOA3.mdx&astroContentModuleFlag=true")]]);
 		
