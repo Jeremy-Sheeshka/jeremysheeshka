cde2f31ce101bf9b15459a8d6e263e4ccf06ca26
Jeremy Sheeshkasudo pacman -S git openssh <your_email@example.com>
2026-03-17 22:58:26 -0700

    math champion push

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -8,5 +8,6 @@ export default new Map([
 ["src/content/post/2026-02-05-etec542-HOA2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-02-05-etec542-HOA2.mdx&astroContentModuleFlag=true")],
 ["src/content/post/2026-02-07-etec544-ch6fullerton.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-02-07-etec544-ch6fullerton.mdx&astroContentModuleFlag=true")],
 ["src/content/post/2026-03-1-etec542-HOA3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-03-1-etec542-HOA3.mdx&astroContentModuleFlag=true")],
-["src/content/post/2026-03-14-etec544-FieldAssignment.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-03-14-etec544-FieldAssignment.mdx&astroContentModuleFlag=true")]]);
+["src/content/post/2026-03-14-etec544-FieldAssignment.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-03-14-etec544-FieldAssignment.mdx&astroContentModuleFlag=true")],
+["src/content/post/2026-03-17-math-champion-game.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2F2026-03-17-math-champion-game.mdx&astroContentModuleFlag=true")]]);
 		
